fe5b05df8de7742f51956d461c8ed7ad5d50d52a
jk <2062816420@qq.com>
2019-12-15 12:14:14 +0800

    version1.0

---
+++ sparkgmall-logger/src/main/java/com/sky/gmall/sparkgmall/logger/controller/LoggerController.java
@@ -0,0 +1,12 @@
+package com.sky.gmall.sparkgmall.logger.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggerController {
+
+
+    public String log(){
+        return null;
+    }
+}
